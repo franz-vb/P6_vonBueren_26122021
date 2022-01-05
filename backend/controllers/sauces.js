77c7e06fa8a7dc10056bd9ce22f69a6509a69ad4
franz von büren <fz.v.bueren@gmail.com>
2022-01-05 10:51:31 +0100

    delete

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -40,45 +40,46 @@ exports.addLike = (req, res, next) => {
   const userId = req.body.userId;
   const like = req.body.like;
   const sauceId = req.params.id;
+  console.log(userId);
   Sauce.findOne({ _id: sauceId })
-      .then(sauce => {
-          // nouvelles valeurs à modifier
-          const newValues = {
-              usersLiked: sauce.usersLiked,
-              usersDisliked: sauce.usersDisliked,
-              likes: 0,
-              dislikes: 0
+    .then((sauce) => {
+      // nouvelles valeurs à modifier
+      const newValues = {
+        usersLiked: sauce.usersLiked,
+        usersDisliked: sauce.usersDisliked,
+        likes: 0,
+        dislikes: 0,
+      };
+      // Différents cas:
+      switch (like) {
+        case 1: // CAS: sauce liked
+          newValues.usersLiked.push(userId);
+          break;
+        case -1: // CAS: sauce disliked
+          newValues.usersDisliked.push(userId);
+          break;
+        case 0: // CAS: Annulation du like/dislike
+          if (newValues.usersLiked.includes(userId)) {
+            // si on annule le like
+            const index = newValues.usersLiked.indexOf(userId);
+            newValues.usersLiked.splice(index, 1);
+          } else {
+            // si on annule le dislike
+            const index = newValues.usersDisliked.indexOf(userId);
+            newValues.usersDisliked.splice(index, 1);
           }
-          // Différents cas:
-          switch (like) {
-              case 1:  // CAS: sauce liked
-                  newValues.usersLiked.push(userId);
-                  break;
-              case -1:  // CAS: sauce disliked
-                  newValues.usersDisliked.push(userId);
-                  break;
-              case 0:  // CAS: Annulation du like/dislike
-                  if (newValues.usersLiked.includes(userId)) {
-                      // si on annule le like
-                      const index = newValues.usersLiked.indexOf(userId);
-                      newValues.usersLiked.splice(index, 1);
-                  } else {
-                      // si on annule le dislike
-                      const index = newValues.usersDisliked.indexOf(userId);
-                      newValues.usersDisliked.splice(index, 1);
-                  }
-                  break;
-          };
-          // Calcul du nombre de likes / dislikes
-          newValues.likes = newValues.usersLiked.length;
-          newValues.dislikes = newValues.usersDisliked.length;
-          // Mise à jour de la sauce avec les nouvelles valeurs
-          Sauce.updateOne({ _id: sauceId }, newValues )
-              .then(() => res.status(200).json({ message: 'Sauce notée !' }))
-              .catch(error => res.status(400).json({ error }))
-      })
-      .catch(error => res.status(500).json({ error }));
-}
+          break;
+      }
+      // Calcul du nombre de likes / dislikes
+      newValues.likes = newValues.usersLiked.length;
+      newValues.dislikes = newValues.usersDisliked.length;
+      // Mise à jour de la sauce avec les nouvelles valeurs
+      Sauce.updateOne({ _id: sauceId }, newValues)
+        .then(() => res.status(200).json({ message: "Sauce notée !" }))
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
 
 exports.modifySauce = (req, res, next) => {
   const sauceObject = req.file
@@ -96,10 +97,10 @@ exports.deleteSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
     .then((sauce) => {
       if (!sauce) {
-        res.status(404).json({ error: new Error("No such Sauce!") });
+        return res.status(404).json({ error: "No such Sauce!"});
       }
       if (sauce.userId !== req.auth.userId) {
-        res.status(400).json({ error: new Error("Unauthorized request!") });
+        return res.status(401).json({ error: "Unauthorized request!" });
       }
       const filename = sauce.imageUrl.split("/images/")[1];
       fs.unlink(`images/${filename}`, () => {
